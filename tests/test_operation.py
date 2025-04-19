fc5f5b72851c662d1ef19e6ff19082bbda2c91a8
Saurabh Sinha <saurabh20051999@gmail.com>
2025-04-19 23:58:05 +0530

    add math fun and test cases

---
+++ tests/test_operation.py
@@ -0,0 +1,11 @@
+from src.math_operation import add,sub
+
+def test_add(a,b):
+    assert add(2,3) == 5
+    assert add(5,3) == 8
+
+def test_sub(a,b):
+    assert sub(2,3) == -1
+    assert sub(5,3) == 2
+    assert sub(8,3) == 5
+    assert sub(7,3) == 4
