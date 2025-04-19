58fff77bf01f48634d0231776254b72ef34d43a9
Saurabh Sinha <saurabh20051999@gmail.com>
2025-04-20 00:29:53 +0530

    fix test_operation file

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,10 +1,10 @@
 from src.math_operation import add,sub
 
-def test_add(a,b):
+def test_add():
     assert add(2,3) == 5
     assert add(5,3) == 8
 
-def test_sub(a,b):
+def test_sub():
     assert sub(2,3) == -1
     assert sub(5,3) == 2
     assert sub(8,3) == 5
